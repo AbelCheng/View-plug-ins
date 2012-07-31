7f49b020a0851d02b717ab92668609b002d87d92
AbelCheng_cp <SND\AbelCheng_cp>
2012-07-31 18:11:19 +0000

    Init check-in

---
+++ View-plug-ins/OracleModel/1-Tables/01-EXTRACT_INTERFACE.sql
@@ -0,0 +1,18 @@
+CREATE TABLE VPI.EXTRACT_INTERFACE
+(
+	INTERFACE_ID	VARCHAR2(32) NOT NULL,
+	UNION_VIEW		VARCHAR2(64),
+	SELECT_LIST		NVARCHAR2(1024),
+	DESCRIPTION_	VARCHAR2(1024),
+
+	CONSTRAINT PK_EXTRACT_INTERFACE PRIMARY KEY (INTERFACE_ID),
+	CONSTRAINT CK_EXTRACT_INTERFACE_ID CHECK (INTERFACE_ID = UPPER(INTERFACE_ID))
+)
+STORAGE (INITIAL 16K NEXT 16K);
+
+COMMENT ON COLUMN VPI.EXTRACT_INTERFACE.INTERFACE_ID
+	IS 'The unique identifier for the interface, consider a naming convention within the enterprise.';
+COMMENT ON COLUMN VPI.EXTRACT_INTERFACE.UNION_VIEW
+	IS 'The name of view which will union all plug-ins under the same interface.';
+COMMENT ON COLUMN VPI.EXTRACT_INTERFACE.SELECT_LIST
+	IS 'The select list in the select statement, every plug-in view of the same interface will follow this signature.';

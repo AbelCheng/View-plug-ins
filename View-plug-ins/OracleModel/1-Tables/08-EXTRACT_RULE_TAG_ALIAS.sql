4fb70fe03d246212e213bfe41e2fac00e41eafda
Abel Cheng <abelcys@gmail.com>
2012-08-11 18:02:29 +0000

    Consolidate code generations into VIEW_CODE_GENERATION.

--- View-plug-ins/OracleModel/1-Tables/08-EXTRACT_RULE_TAG_ALIAS.sql
+++ View-plug-ins/OracleModel/1-Tables/08-EXTRACT_RULE_TAG_ALIAS.sql
@@ -28,37 +28,37 @@ COMMENT ON TABLE EXTRACT_RULE_TAG_ALIAS
 	IS 'This table defines alias of TAG$## columns for each interface.';
 
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.RULE_VIEW
-	IS 'The view of EXTRACT_RULE (subset of VIEW_EXTRACT_RULE) which will assign intelligible alias to every appliable TAG$## columns.';
+	IS 'The updatable view of EXTRACT_RULE which will assign intelligible alias to every appliable TAG$## columns.';
 
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$01
-	IS 'Alias of TAG$01 column (if appliable).';
+	IS 'Alias of EXTRACT_RULE.TAG$01 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$02
-  	IS 'Alias of TAG$02 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$02 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$03
-  	IS 'Alias of TAG$03 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$03 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$04
-  	IS 'Alias of TAG$04 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$04 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$05
-  	IS 'Alias of TAG$05 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$05 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$06
-  	IS 'Alias of TAG$06 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$06 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$07
-  	IS 'Alias of TAG$07 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$07 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$08
-  	IS 'Alias of TAG$08 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$08 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$09
-  	IS 'Alias of TAG$09 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$09 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$10
-  	IS 'Alias of TAG$10 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$10 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$11
-  	IS 'Alias of TAG$11 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$11 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$12
-  	IS 'Alias of TAG$12 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$12 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$13
-  	IS 'Alias of TAG$13 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$13 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$14
-  	IS 'Alias of TAG$14 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$14 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$15
-  	IS 'Alias of TAG$15 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$15 column (if appliable).';
 COMMENT ON COLUMN EXTRACT_RULE_TAG_ALIAS.TAG$16
-  	IS 'Alias of TAG$16 column (if appliable).';
+  	IS 'Alias of EXTRACT_RULE.TAG$16 column (if appliable).';
